fd3139909442425fc507d47a018db218a1d91954
hossamgado <hossammahmoudgado@gmail.com>
2025-09-03 08:39:14 +0300

    refresh note list

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/widgets/note_view_body.dart';
-
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
