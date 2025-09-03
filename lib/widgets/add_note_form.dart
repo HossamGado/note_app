1231666f70ce5cf6117ec625e5637bbf9a47fbbe
hossamgado <hossammahmoudgado@gmail.com>
2025-09-03 11:06:06 +0300

    create Colors list view

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
 import 'package:note_app/models/note_model.dart';
+import 'package:note_app/widgets/colors_list_view.dart';
 import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 import 'package:intl/intl.dart';
@@ -41,6 +42,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: "content",
           ),
           SizedBox(height: 32),
+         const ColorsListView(),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomButton(
---
+++ lib/widgets/colors_list_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ColorsItem extends StatelessWidget {
+  const ColorsItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(radius: 38, backgroundColor: Colors.blue);
+  }
+}
+
+class ColorsListView extends StatelessWidget {
+  const ColorsListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 76,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ColorsItem();
+        },
+      ),
+    );
+  }
+}
